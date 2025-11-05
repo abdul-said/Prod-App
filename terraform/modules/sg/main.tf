3966b58778aef3869a8125efc54e3ce1ef33f157
abdul-said <abdul-said@outlook.com>
2025-11-05 22:54:50 +0000

    updated sg

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -25,6 +25,22 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
   to_port           = var.http_port
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
+  security_group_id = aws_security_group.app_sg.id
+  cidr_ipv4         = var.cidr_ipv4
+  from_port         = 443
+  ip_protocol       = var.ip_protocol_tcp
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv6" {
+  security_group_id = aws_security_group.app_sg.id
+  cidr_ipv6         = var.cidr_ipv6
+  from_port         = 443
+  ip_protocol       = var.ip_protocol_tcp
+  to_port           = 443
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.app_sg.id
   cidr_ipv4         = var.cidr_ipv4
